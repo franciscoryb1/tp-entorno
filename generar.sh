0f184f72d412c27a9eece5eaa3fc9ab14bce92e8
franciscoryb1 <franciscoryb1@hotmail.com>
2023-07-15 16:37:59 -0300

    Modificacion de generar para que asigne los nombres de personas

--- generar.sh
+++ generar.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 if [ $# -eq 0 ]; then
- 	 echo "Error: debe especificar la cantidad de imágenes a generar como argumento."
- 	 exit 1
+  echo "Error: debe especificar la cantidad de imágenes a generar como argumento."
+  exit 1
 fi
+
 cantidad_imagenes=$1
-directorio_destino="./descargas"
-if [ ! -d "$directorio_destino" ]; then
- 	 mkdir -p "$directorio_destino"
+
+DIR_DESTINO="./descargas"
+if [ ! -d "$DIR_DESTINO" ]; then
+  mkdir -p "$DIR_DESTINO"
 fi
+
+URL="https://source.unsplash.com/random/900%C3%97700/?person"
+
 
 for ((i = 1; i <= cantidad_imagenes; i++)); do
-  	nombre_archivo="imagen_$i.jpg"
-	url="https://source.unsplash.com/random/900%C3%97700/?person"
-	redirect_url=$(curl -s -o /dev/null -w "%{redirect_url}" "$url")
-	curl -o "$directorio_destino/$nombre_archivo" "$redirect_url"
-	echo "Imagen descargada: $nombre_archivo"
-  	sleep 1
+	NOMBRE=$(shuf nombres.csv | grep '^[A-Z]' | head -n 1 | cut -d ' ' -f '1')
+	wget $URL -O "./$DIR_DESTINO/$NOMBRE"
+	sleep 1
 done
-
+exit 0
