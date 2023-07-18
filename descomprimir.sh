0f076f8ac3622003c1c5d3f6aba8febcea4f3976
marianosancho <msancho.almenar@gmail.com>
2023-07-18 14:08:09 -0300

    Avance en descomprimir.sh

--- descomprimir.sh
+++ descomprimir.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/bash
 
-if [ "$#" -ne 2 ]; then 
-
-	echo "No se proporcionarion dos archivos. Genere primero las imagenes"
+if [ "$#" != 2 ]; then 
+	echo "Error: Deben propocionarse el archivo comprimido y un archivo con la suma de verificacion."
 	exit 1
-
 fi 	
 
-ZIP="$1" 
-SUMA=$(<"$2")
-SUMA_VERIF=$(md5sum "$ZIP")
+
+if [[ -e "$1" && -e "$2" ]]; then
+    ZIP="$1" 
+    SUMA=$(<"$2")
+    SUMA_VERIF=$(md5sum "$ZIP" | cut -d ' ' -f '1')
+else
+    echo "Error: Alguno de los archivos pasados no existe."
+    exit 1
+fi
 
 
 if [[ "$SUMA" == "$SUMA_VERIF" ]]; then 
 
-	echo "Las sumas coinciden" 
+	unzip $ZIP -d imagenes
+	rm ./"$ZIP"
+	rm ./"$2"
+	#./menu.sh
+    echo "¡Descomprimido con exito!"
 	exit 0
-
+	
 else 
-	echo "Las sumas no coinciden" 
+	echo "Las sumas de verificacion no coinciden"
+	#./menu.sh
 	exit 1
-
-fi 		
+fi	
