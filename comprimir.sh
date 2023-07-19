d41c22ae78105383b134687ad5fcc12193dc3fa9
santi_giaveno <santi_giaveno@TpEntorno.myguest.virtualbox.org>
2023-07-19 14:08:31 -0300

    Creacion de comprimir.sh

---
+++ comprimir.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+
+
+if [ -e "$1" ]; then
+	ls "$1" > nombres
+	ls "$1" | grep -oE '^[A-Z][a-z]+$' > nombres_validos
+	ls "$1" | grep -c '^[A-Z][a-z]*a$'> nombres_finalizados_con_a
+else
+    echo "Error: No existe la carpeta."
+exit 1
+fi
+
+mv nombres "$1"
+mv nombres_validos "$1"
+mv nombres_finalizados_con_a "$1"
+cd "$1"
+
+zip -r ../salida.zip *
+cd ..
+
+md5sum salida.zip | cut -d ' ' -f '1'>suma_verificacion_salida
+rm -r "$1"
+echo "Se comprimieron las imagenes."
+exit 0
