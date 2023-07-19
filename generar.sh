883d8b34815ece8c5e7eeaf83bcc8ee9ffc0549d
marianosancho <msancho.almenar@gmail.com>
2023-07-19 18:09:25 -0300

    Retorno a menu y arreglo de codigo

--- generar.sh
+++ generar.sh
@@ -2,6 +2,7 @@
 
 if [ $# -eq 0 ]; then
   echo "Error: debe especificar la cantidad de imágenes a generar como argumento."
+  ./menu.sh
   exit 1
 fi
 
@@ -10,20 +11,21 @@ cantidad_imagenes=$1
 DIR_DESTINO="./imagenes"
 if [ ! -d "$DIR_DESTINO" ]; then
   mkdir -p "$DIR_DESTINO"
+  
 fi
 
 URL="https://thispersondoesnotexist.com/"
 
 cd $DIR_DESTINO
 for ((i = 1; i <= cantidad_imagenes; i++)); do
-	NOMBRE=$(shuf ../nombres.csv | grep '^[A-Z]' | head -n 1 | cut -d ' ' -f '1' | cut -d "," -f "1")
+	NOMBRE=$(sort -R ../nombres.csv | grep '^[A-Z]' | head -n 1 | cut -d ' ' -f '1' | cut -d "," -f "1")
 	wget $URL -O "./$NOMBRE"
 	sleep 1
 done
 
 zip -r ../imagenes.zip *
 cd ../
-echo $(sha256sum imagenes.zip | cut -d ' ' -f '1')>suma_verificacion
+echo $(md5sum imagenes.zip | cut -d ' ' -f '1')>suma_verificacion
 rm -r imagenes
-
+./menu.sh
 exit 0
